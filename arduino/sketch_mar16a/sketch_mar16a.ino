4b41d5010a7e883431648761bef24e43fbf7e41b
Nagarjuna GS <nagarjunags2014@gmail.com>
2023-03-24 22:28:42 +0530

    final commit

--- arduino/sketch_mar16a/sketch_mar16a.ino
+++ arduino/sketch_mar16a/sketch_mar16a.ino
@@ -1,14 +1,10 @@
-#define MASK(x) ((unsigned char)(1<<x))
+
 void setup() {
-  // put your setup code here, to run once:
-  DDRB |=MASK(5);//make the pin b as output
+
 }
 
 void loop() 
 {
-    PORTB |= MASK(5);
-    delay(5000);
-    PORTB &= MASK(5);
-    delay(5000);
+
 
 }
